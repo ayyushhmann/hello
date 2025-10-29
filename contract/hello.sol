a961f92862a22cd95478c8052f41acb7f842f13c
ayyushhmann <ayushmanp108@gmail.com>
2025-10-29 14:24:15 +0530

    Update hello.sol

--- contract/hello.sol
+++ contract/hello.sol
@@ -1,1 +1,30 @@
 
+pragma solidity ^0.8.0;
+
+
+contract XYZContract {
+  
+    string public message;
+
+    
+    address public owner;
+
+    
+    constructor(string memory _message) {
+        message = _message;
+        owner = msg.sender; // msg.sender is the deployer
+    }
+
+    
+    function setMessage(string memory _newMessage) public {
+        
+        require(msg.sender == owner, "Not authorized");
+        message = _newMessage;
+    }
+
+    
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
+
